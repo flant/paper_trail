8000b2846e38fa18455caa1dd6acdb6201b6df74
Gennady.Kalashnikov <Gennady.Kalashnikov@mgaldoru-third>
2012-08-21 22:05:39 +0400

    fixed migrations names in generator

--- lib/generators/paper_trail/install_generator.rb
+++ lib/generators/paper_trail/install_generator.rb
@@ -10,11 +10,11 @@ module PaperTrail
     source_root File.expand_path('../templates', __FILE__)
     class_option :with_changes, :type => :boolean, :default => false, :desc => "Store changeset (diff) with each version"
 
-    desc 'Generates (but does not run) a migration to add a versions table.'
+    desc 'Generates (but does not run) a migration to add a paper_trail_versions table.'
 
     def create_migration_file
-      migration_template 'create_versions.rb', 'db/migrate/create_versions.rb'
-      migration_template 'add_object_changes_column_to_versions.rb', 'db/migrate/add_object_changes_column_to_versions.rb' if options.with_changes?
+      migration_template 'create_versions.rb', 'db/migrate/create_paper_trail_versions.rb'
+      migration_template 'add_object_changes_column_to_paper_trail_versions.rb', 'db/migrate/add_object_changes_column_to_paper_trail_versions.rb' if options.with_changes?
     end
   end
 end
