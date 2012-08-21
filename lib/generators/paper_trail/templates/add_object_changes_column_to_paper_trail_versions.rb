d991553986d9322228a81813aee0865f09c8cc30
Gennady.Kalashnikov <Gennady.Kalashnikov@mgaldoru-third>
2012-08-21 21:49:38 +0400

    renamed default table from 'versions' to 'paper_trail_versions'

---
+++ lib/generators/paper_trail/templates/add_object_changes_column_to_paper_trail_versions.rb
@@ -0,0 +1,9 @@
+class AddObjectChangesColumnToPaperTrailVersions < ActiveRecord::Migration
+  def self.up
+    add_column :paper_trail_versions, :object_changes, :text
+  end
+
+  def self.down
+    remove_column :paper_trail_versions, :object_changes
+  end
+end
